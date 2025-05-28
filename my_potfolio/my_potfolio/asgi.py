6971af03b1ae9f18d85a00d55654275f29312c38
unknown <ironthroneking1@gmail.com>
2025-05-28 03:07:51 +0100

    my portfolio

--- my_potfolio/my_potfolio/asgi.py
+++ my_potfolio/my_potfolio/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
+8
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_potfolio.settings')
 
 application = get_asgi_application()
